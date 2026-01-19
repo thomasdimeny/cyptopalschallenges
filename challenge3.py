9503c0aaf5917f5cb5535baeee07fc764a067b65
thomasdimeny <crow@crows-MacBook-Air.local>
2026-01-19 14:56:47 -0600

    finished challenge 3

--- challenge3.py
+++ challenge3.py
@@ -13,29 +13,33 @@ How? Devise some method for "scoring" a piece of English plaintext. Character fr
 # convert hex to ascii
 
 hexStr = "1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"
-asciiConversion = bytes.fromhex(hexStr)
-stringConversion = asciiConversion.decode()
 
-# make map of characters/occurrences
-occurrences = {}
-for char in stringConversion:
-    if char not in occurrences:
-        occurrences[char] = 1
-    else:
-        occurrences[char] += 1
+#convert to bytes object to work with
+data = bytes.fromhex(hexStr)
 
-key = max(occurrences)
-hexKey = key.encode("utf-8").hex()
-alphabet = "abcdefghijklmnopqrstuvwxyz"
-alphabet = alphabet.encode("utf-8").hex()
+# initialize empty solution
+candidate = ""
+solution = {}
+highScore = 0
+def scoring(byteString: bytes):
+     #this 
+    myByte = b"\x20"
+    
+    return byteString.count(myByte)
 
+#xor each hex char from 1, 255
 
-solution = ""
-
-for i in range(len(alphabet)):
-    for j in range(len(hexStr)):
-        hexChar = int(alphabet[i], 16) ^ int(hexStr[j], 16)
-        solution += f"{hexChar:02x}"
-    print(bytes.fromhex(solution))
-    print("\n")
-    solution = ""
+for i in range(1,255):
+    #cast to bytes here to force map to iterate over each element in data, 
+    #cast i to byte to xor properly 
+    candidate = bytes(map(lambda x: x ^ i, data))
+    score = scoring(candidate)
+    if score > highScore: 
+        highScore = score
+        candidate = candidate.decode("latin-1")
+        #we'll return a solution of most viable c
+        solution[candidate] = highScore
+    #decode since we are already working with bytes
+    
+print(solution)
+   
